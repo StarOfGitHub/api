bbf1fd3f7e233b6d37c51be09351224a24abd18e
StarOfGitHub <sparklingstar@gmail.com>
2023-11-09 21:25:25 -0800

    second commit

--- index.js
+++ index.js
@@ -16,16 +16,28 @@ app.listen(port, () => {
 
 function sendPushNotification(alt_message) {
   const admin = require('firebase-admin')
+  const apn = require('apn');
+
   const serviceAccount = require('./qbit-prod-firebase-adminsdk-vwacc-fef1c77e50.json')
+
+  const apnProvider = new apn.Provider({
+    token: {
+      key: 'SubscriptionKey_6T5S995V2J.p8', // Path to your APNs Auth Key
+      keyId: 'push_notification', // Your APNs Auth Key ID
+      teamId: 'X9NH7R32V4', // Your Team ID
+    },
+    production: false, // Set to true for production environment
+  });
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   })
 
-  const notification = {
-    title: 'New Notification',
-    body: alt_message || 'Hello, this is a push notification!', // Use the provided message or a default message
-  };
+  const notification = new apn.Notification();
+
+  
+  notification.title = 'New Notification';
+  notification.body = alt_message || 'Hello, this is a push notification!'; // Use the provided message or a default message
 
   const topic = 'all'; // Send to all devices subscribed to the topic 'all'
 
@@ -34,6 +46,11 @@ function sendPushNotification(alt_message) {
     topic,
   };
 
+  apnProvider.send(notification, deviceToken).then((result) => {
+    console.log('Push notification sent:', result.sent);
+    console.log('Failed tokens:', result.failed);
+  });
+
   admin.messaging().send(message)
     .then((response) => {
       console.log('Successfully sent push notification:', response);
@@ -41,4 +58,6 @@ function sendPushNotification(alt_message) {
     .catch((error) => {
       console.error('Error sending push notification:', error);
     });
+
+  sendPushNotification(deviceToken, title, body);
 }
